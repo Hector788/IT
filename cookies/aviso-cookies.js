b2488700def0109ec56612aa707384a96f247745
Hector De Hoyos <hectordehoyos053@gmail.com>
2023-01-31 09:20:50 -0600

    Actualizando pagina

--- cookies/aviso-cookies.js
+++ cookies/aviso-cookies.js
@@ -4,18 +4,40 @@ const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
 
 dataLayer = [];
 
-if(!localStorage.getItem('cookies-aceptadas')){
-	avisoCookies.classList.add('activo');
-	fondoAvisoCookies.classList.add('activo');
+if (!localStorage.getItem('cookies-aceptadas')) {
+    avisoCookies.classList.add('activo');
+    fondoAvisoCookies.classList.add('activo');
 } else {
-	dataLayer.push({'event': 'cookies-aceptadas'});
+    dataLayer.push({ 'event': 'cookies-aceptadas' });
 }
 
 botonAceptarCookies.addEventListener('click', () => {
-	avisoCookies.classList.remove('activo');
-	fondoAvisoCookies.classList.remove('activo');
+    avisoCookies.classList.remove('activo');
+    fondoAvisoCookies.classList.remove('activo');
 
-	localStorage.setItem('cookies-aceptadas', true);
+    localStorage.setItem('cookies-aceptadas', true);
 
-	dataLayer.push({'event': 'cookies-aceptadas'});
-});	
+    dataLayer.push({ 'event': 'cookies-aceptadas' });
+});
+
+
+
+document.getElementById("likeButton").addEventListener("click", function() {
+    // código para contar los clics aquí
+});
+
+let likeCount = 0;
+
+// cargar la información de almacenamiento local si existe
+if (localStorage.getItem("likeCount")) {
+    likeCount = parseInt(localStorage.getItem("likeCount"));
+}
+
+document.getElementById("likeButton").innerHTML = " ME GUSTA(" + likeCount + ")";
+
+// asignar manejador de eventos de clic al botón
+document.getElementById("likeButton").addEventListener("click", function() {
+    likeCount++;
+    document.getElementById("likeButton").innerHTML = "ME GUSTA(" + likeCount + ")";
+    localStorage.setItem("likeCount", likeCount);
+});
